e96b62459bea0b4efed8981febd36b45846b87e2
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-08 17:46:40 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def name
+    @name
+  end
+  def self.all
+    @@all.each do |dogs|
+      puts dogs.name
+    end
+  end
+  def self.clear_all
+    @@all = []
+  end
+  
+end
